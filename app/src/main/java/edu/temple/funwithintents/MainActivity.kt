fa0ca037d9d9f9cec1ba347a9e4c8a974b2be8d5
Sean Gauv <tuu21205@temple.edu>
2024-11-07 11:54:15 -0500

    attempted to implement text sharing

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,9 +1,11 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +15,17 @@ class MainActivity : AppCompatActivity() {
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
 
+        val textMessage = editText
+
+        val sendIntent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, textMessage.toString())
+            type = "text/plain"
+        }
+
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            startActivity(sendIntent)
         }
     }
 }
